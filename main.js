80d642466124a3297fa457b861b33fcf7a302057
WaysideTester <dylanmurray2119@gmail.com>
2018-09-06 20:23:12 -0500

    completed data entry

---
+++ main.js
@@ -0,0 +1,13 @@
+const language = {
+        merry: ['merry','gaja','fröhlich','メリー'],
+        christmas: ['christmas','Kristnasko','Weihnachten','クリスマス'],
+        and: ['and','kaj','und','そして'],
+        happy: ['happy','feliĉa','glücklich','ハッピー'],
+        news: ['new','nova','Neu','新しい'],
+        year:['year','jaro','Jahr','年']
+};
+
+console.log(language.and[2]);
+console.log(language.merry[3]);
+console.log(language.christmas[1]);
+console.log(language.year[0]);
