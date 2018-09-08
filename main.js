d1d478c92a816930a7369f902e5a8aad41d69c0d
WaysideTester <dylanmurray2119@gmail.com>
2018-09-08 10:54:25 -0500

    button works. Needs translation functionality

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
+// Prints input to DOM
 const printToDOM = (divId, infoToPrint) => {
   const selectedDiv = document.getElementById(divId);
   selectedDiv.innerHTML = infoToPrint;
-}
+};
 
 const language = {
         merry: ['merry','gaja','fröhlich','メリー'],
@@ -12,12 +13,25 @@ const language = {
         year:['year','jaro','Jahr','年']
 };
 
-for (const prop in language) {
-    console.log(`language.${prop} = ${language[prop]}`);
-  }
+// turn languagne object keys into an array
+const keyArray = Object.keys(language);
 
-// language.christmas.forEach(function(item, index, array) {
-//     console.log(index, item, array);
-// });
+// buttons
+const espirantobutt = document.getElementById('Esperanto');
+const germanbutt = document.getElementById('German');
+const japanesebutt = document.getElementById('Japanese');
+// end buttons
 
-printToDOM ('printHere', 'I function');
+// function to pull info from textarea and check values in languages object
+const getTextValue = () => {
+  let toTranslate = document.getElementById('toTranslate');
+  let translateText = toTranslate.value;
+  console.log(translateText);
+};
+
+
+
+
+espirantobutt.addEventListener('click',getTextValue);
+
+printToDOM ('printHere', keyArray);
